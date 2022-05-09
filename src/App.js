b1ae63244c0e9825ad27f6adaa68df5cec4beeee
je-young <jey2965@gmail.com>
2022-05-09 21:54:43 +0900

    for props.topics.length lis.push

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function Header(props) {
 }
 
 function Nav(props) {
-  const lis = [
-    <li><a href="/read/1">html</a></li>,
-    <li><a href="/read/2">css</a></li>,
-    <li><a href="/read/3">javascript</a></li>
-  ]
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
       {lis}
