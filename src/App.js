40ebf84f44edaa4d493061b2baf0c2648c56b9c3
je-young <jey2965@gmail.com>
2022-05-10 00:06:02 +0900

    useState 로 Nav content 구현

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/' + t.id} onClick={event => {
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a></li>)
   }
   return <nav>
@@ -35,10 +35,8 @@ function Article(props) {
 }
 
 function App() {
-  // const _mode = useState('WELCOME');
-  // const mode = _mode[0];
-  // const setMode = _mode[1];
   const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
@@ -48,15 +46,23 @@ function App() {
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if (mode === 'READ') {
-    content = <Article title="Read" body="Hello, Read"></Article>
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
   }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
         setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id) => {
+      <Nav topics={topics} onChangeMode={(_id) => {
         setMode('READ');
+        setId(_id);
       }}></Nav>
       {content}
     </div>
